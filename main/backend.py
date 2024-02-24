e5411a4467beb3be509865ddaa084bd4be8446b4
limejack <82292253+limejack@users.noreply.github.com>
2024-02-24 15:39:46 -0500

    Updated Backend.py
    Basically just function to optimize

--- main/backend.py
+++ main/backend.py
@@ -1,5 +1,35 @@
 class Teacher():
-    def __init__(self,name,room_number):
+    def __init__(self,name,room_number,periods):
         self.name = name
         self.room_number = room_number
-        self.period
+        self.periods = periods #periods teaching
+class Class():
+    def __init__(self,name,number,maxcapacity):
+        self.name = name
+        self.number = number
+        self.max_capacity = maxcapacity
+        self.students = []
+class DesiredSchedule():
+    def __init__(self,wants,secondary):
+        self.wants = wants
+        self.secondary = secondary
+
+    def dist(self,otherSchedule):
+        out = 0
+        for i in otherSchedule:
+            if i in self.wants:
+                out += 1
+            elif i in self.secondary:
+                out += 0.5
+            else:
+                out += 0
+        return out
+
+
+def cost(currentSchedule,desiredSchedule,dist):
+    cost = 0
+    for i in currentSchedule:
+        cost += desiredSchedule.dist(currentSchedule)
+    for i in range(len(currentSchedule)-1):
+        cost += dist[currentSchedule[i]][currentSchedule[i+1]]
+    return cost
