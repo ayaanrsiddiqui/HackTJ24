b911018a2a123d57f155ab84f14f64f8dc7a54f4
Ayaan Siddiqui <ayaanrsiddiqui@gmail.com>
2024-02-24 17:17:30 -0500

    Connected class choice page to class list

--- app.py
+++ app.py
@@ -3,17 +3,23 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+classes = open("main/classes.txt", "r")
+classnames = []
+for line in classes:
+    classnames.append(line.strip())
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', c = classnames)
 
 @app.route('/submit', methods=['POST'])
 def submit():
     first_choices = []
     second_choices = []
     for i in range(1, 8):
-        first_choices.append(request.form[f'class{i}_first_choice'])
-        second_choices.append(request.form[f'class{i}_second_choice'])
+        first_choices.append(classnames[int(request.form[f'class{i}_first_choice'])])
+        second_choices.append(classnames[int(request.form[f'class{i}_second_choice'])])
     return f'First choice classes: {first_choices} \n Second choice classes: {second_choices}'
 
 if __name__ == '__main__':
