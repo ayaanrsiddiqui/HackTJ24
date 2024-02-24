25f79fa0e94344acd5e73e6fdfdb9dfc49d8f727
limejack <82292253+limejack@users.noreply.github.com>
2024-02-24 18:19:08 -0500

    Mo Quantum mo problems

--- main/quantumTime.py
+++ main/quantumTime.py
@@ -1,6 +1,20 @@
+def getDists():
+    with open('main/distanceMatrix.txt') as infile:
+        return eval(infile.read())
+def getClasses():
+    return [('physics',1,0),('physics',2,0),('lit',2,10)]
 from dwave.samplers import SimulatedAnnealingSampler
 
-mutuallyExlusive = [(0,1),(1,2)]
+classes = getClasses()
+
+mutuallyExclusive = []
+for i in range(len(classes)):
+    for j in range(len(classes)):
+        if i == j:
+            continue
+        if classes[i][0] == classes[j][0] or classes[i][1] == classes[j][1]:
+            mutuallyExclusive.append((i,j))
+
 classesTotal = 3
 individualTotals = 2
 
@@ -15,9 +29,14 @@ for i in range(classesTotal):
 for i in range(classesTotal):
     qubo[(i,i)] -= 2*T*M
 
+distMat = getDists()
 
-
-for i in mutuallyExlusive:
+for i in range(len(classes)):
+    for j in range(len(classes)):
+        if abs(classes[i][1]-classes[j][1]) == 1:
+            qubo[(i,j)] += distMat[classes[i][2]][classes[j][2]]
+for i in mutuallyExclusive:
     qubo[i] += M
+
 sampler = SimulatedAnnealingSampler()
 print(sampler.sample_qubo(qubo))
